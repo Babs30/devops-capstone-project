a41bca4694b57b022996cd2aaa5cb2774fb3ebaf
Babs30 <ainavince@gmail.com>
2025-11-22 02:05:51 -0500

    Added security headers

--- service/routes.py
+++ service/routes.py
@@ -11,6 +11,7 @@ from service.common import status  # HTTP Status Codes
 from . import app  # Import Flask application
 
 
+
 ############################################################
 # Health Endpoint
 ############################################################
